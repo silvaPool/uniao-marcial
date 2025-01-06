39ae621adf1cd25465286f28409a9eb8ff2eb0ae
silvaPool <silva.friends22@gmail.com>
2025-01-06 09:23:47 -0300

    registro com o google

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,49 +1,78 @@
 import { useState } from "react";
 
 const SignupWithEmail = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSignup = async (e) => {
-        e.preventDefault();
-        if (!email || !password) {
-            console.error("Provide Email and Password");
-            return;
-        }
-        try {
-            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-            const user = userCredential.user;
-            console.log(user);
-        } catch (error) {
-            const errorCode = error.errorCode;
-            const errorMessage = error.message;
-            console.log("errorCode:", errorCode, "errorMessage:", errorMessage);
-        }
-    };
-    
-    return (
-        <form onSubmit={handleSignup}>
-            <div>
-                <label>Email:</label>
-                <input
-                 type="text"
-                 value={email}
-                 onChange={(e) => setEmail(e.target.value)}
-                 required
-                />
-            </div>
-            <div>
-                <label>Password:</label>
-                <input
-                 type="password"
-                 value={password}
-                 onChange={(e) => setPassword(e.target.value)}
-                 required
-                />
-            </div>
-            <button type="submit">Sign Up</button>
-        </form>
-    );
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    if (!email || !password) {
+      console.error("Provide Email and Password");
+      return;
+    }
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      console.log(user);
+    } catch (error) {
+      const errorCode = error.errorCode;
+      const errorMessage = error.message;
+      console.log("errorCode:", errorCode, "errorMessage:", errorMessage);
+    }
+  };
+
+  const handleGoogleSignup = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+
+      if (!credential) {
+        console.error("Error in user Credential");
+        return;
+      }
+      const token = credential.accessToken;
+      const user = result.user;
+      console.log(user, token);
+    } catch (error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        console.error("Google Signup Error:", errorCode, errorMessage, email, credential);
+    }
+  };
+
+  return (
+    <div>
+    <form onSubmit={handleSignup}>
+      <div>
+        <label>Email:</label>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit">Sign Up</button>
+    </form>
+    <button onClick={handleGoogleSignup}>Sign Up with Google</button>
+    </div>
+  );
 };
 
 export default SignupWithEmail;
