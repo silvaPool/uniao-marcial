9687446ba7d8948754d9e0a76e547bd32b8ff01b
silvaPool <silva.friends22@gmail.com>
2025-01-10 09:19:34 -0300

    funcoes implementas no context

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import { auth } from "../services/firebaseConfig";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 export const AuthContext = createContext({});
 
@@ -19,8 +20,36 @@ export const Auth = ({ children }) => {
 
     }, []);
 
-    
+    async function login(email, senha) {
+        try {
+            const response = await signInWithEmailAndPassword(authRef, email, senha);
 
+            setUser(response.user);
+            localStorage.setItem("@AccessToken", response.user.accessToken);
+            localStorage.setItem("@User", JSON.stringify(response.user));
+
+            return {success: true, user: response.user};
+        } catch (error) {
+            return {success: false, message: error.message};
+        }
+    }
+
+    async function signUp(email, senha) {
+        try {
+            const response = await createUserWithEmailAndPassword(authRef, email, senha);
+
+            return response;
+        } catch (error) {
+            return error.message;
+        }
+    }
+
+    function logout() {
+        localStorage.removeItem("@AccessToken");
+        localStorage.removeItem("@User");
+
+        setUser(null);
+    }
 
     return (
         <AuthContext.Provider>
