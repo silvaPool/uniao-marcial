d4a345c5d4da6abee967a58f70c33d3e5540d423
silvaPool <silva.friends22@gmail.com>
2025-01-15 09:06:47 -0300

    componente do material implementado no layout do usuario para mais opcoes de uso

--- src/pages/PaginaUsuario.jsx
+++ src/pages/PaginaUsuario.jsx
@@ -7,6 +7,7 @@ import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import Teste from "./Teste";
+import BasicMenu from "./FuncoesLayoutUsuario";
 
 export default function PaginaUsuario() {
   return (
@@ -26,7 +27,7 @@ export default function PaginaUsuario() {
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               News
             </Typography>
-            <Button color="inherit">Login</Button>
+            <BasicMenu />
           </Toolbar>
         </AppBar>
       </Box>
