0fd9a92f223726a929a8e0db2c60b09a3d5c03d8
silvaPool <silva.friends22@gmail.com>
2025-02-20 10:43:07 -0300

    componente para permitir o acesso ao historico

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { StrictMode } from "react";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Auth0Provider>
+        <App />
+      </Auth0Provider>
+    </BrowserRouter>
   </StrictMode>
 );
