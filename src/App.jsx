5bb894ad72ee0c3c7acbfa38caedc6f1ce98aab2
silvaPool <silva.friends22@gmail.com>
2025-02-24 08:48:04 -0300

    rotas modificadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import "./App.css";
 import LoginButton from "./components/LoginButton";
-
-
+import { Route, Routes } from "react-router";
 
 function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <>
-      
-      <LoginButton />
-      
+      <div>
+        <NavBar />
+
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/external-api" element={<ExternalApi />} />
+          </Routes>
+        </div>
+      </div>
     </>
   );
 }
